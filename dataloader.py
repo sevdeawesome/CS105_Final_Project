ac41ec6ca26de6652af929c1c8502f417c8ed44b
Severin Field <sevdeawesome@gmail.com>
2022-03-05 15:55:15 -0800

    data loader

---
+++ dataloader.py
@@ -0,0 +1,13 @@
+
+from skimage import io
+
+
+## INPUT PARAMS
+IMG_WIDTH = 1200
+IMG_HEIGHT = 600
+
+
+def load_data(filename):
+    image = io.imread('img/Thinking-of-getting-a-cat.png')
+    image_data = (image).reshape(1200*600, 3)
+    return image_data
